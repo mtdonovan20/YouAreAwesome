0982f67705fb671b436ea97f079590c9e451688b
user144301 <user144301@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-12 22:49:07 -0500

    Initial Commit

---
+++ You Are AwesomeTests/You_Are_AwesomeTests.swift
@@ -0,0 +1,34 @@
+//
+//  You_Are_AwesomeTests.swift
+//  You Are AwesomeTests
+//
+//  Created by user144301 on 11/12/18.
+//  Copyright © 2018 user144301. All rights reserved.
+//
+
+import XCTest
+@testable import You_Are_Awesome
+
+class You_Are_AwesomeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
